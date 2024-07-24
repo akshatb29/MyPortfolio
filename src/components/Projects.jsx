a7becceffd60c8d910adb5395ee4addcd9314c9a
akshat <38420033+akshatb29@users.noreply.github.com>
2024-07-25 02:31:18 +0530

    Update Projects.jsx
    formatted projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,50 +9,49 @@ const Projects = () => {
 
                 <div className='text-white grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 text-center'>
                     <div className='bg-neutral-900 p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105'>
-                        <h3 className='flex items-center justify-start font-bold text-.5xl'>
-                            <a href="https://github.com/your-repo-link" target="_blank" >
-                                <img src={github1} alt="GitHub" className="w-10 h-10 mr-4 mix-blend-normal" />
+                        <h3 className='flex items-center justify-start font-bold text-1.5xl'>
+                            <a href="https://github.com/your-repo-link" target="_blank">
+                                <img src={github1} alt="GitHub" className="w-10 h-10 mr-4" />
                             </a>
                             Encryption Decryption using Sockets
                         </h3>
                         <div className='font-serif mt-2 text-sm overflow-y-auto h-auto py-3'>
-                            - Server sends ENCRYPTED file to client upon request, the file is DECRYPYED using private key
-                            - Used Secure Socket Layering (SSL) in TCP
-                            - Made using Python
-
+                            <p className='text-left'>- Server sends encrypted file to client upon request, the file is decrypted using private key</p>
+                            <p className='text-left'>- Used Secure Socket Layering (SSL) in TCP</p>
+                            <p className='text-left'>- Made using Python</p>
                         </div>
                     </div>
 
                     <div className='bg-neutral-900 p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105'>
                         <h3 className='flex items-center justify-start font-bold text-1.5xl'>
-                            <a href="https://github.com/your-repo-link" target="_blank" >
+                            <a href="https://github.com/your-repo-link" target="_blank">
                                 <img src={github1} alt="GitHub" className="w-10 h-10 mr-4" />
                             </a>
                             Urban Applause
                         </h3>
-                        <div className='font-serif mt-2 text-sm overflow-y-auto h-full py-3'>
-                            - Web Application for booking urban services that makes use of CRUD operations with friendly UI.
-                            - Users log in, select services, add to a dynamic cart, and see the total cost.
-                            - Packages of Services and User login data stored in MongoDB.
-                            - Made using React, Node.js, MongoDB
+                        <div className='font-serif mt-2 text-sm overflow-y-auto h-auto py-3'>
+                            <p className='text-left'>- Web Application for booking urban services that makes use of CRUD operations with friendly UI.</p>
+                            <p className='text-left'>- Users log in, select services, add to a dynamic cart, and see the total cost.</p>
+                            <p className='text-left'>- Packages of Services and User login data stored in MongoDB.</p>
+                            <p className='text-left'>- Made using React, Node.js, MongoDB</p>
                         </div>
                     </div>
 
                     <div className='bg-neutral-900 p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105'>
-                        <h3 className='flex items-center justify-start font-bold text-1.5xl '>
-                            <a href="https://github.com/your-repo-link" target="_blank" >
+                        <h3 className='flex items-center justify-start font-bold text-1.5xl'>
+                            <a href="https://github.com/your-repo-link" target="_blank">
                                 <img src={github1} alt="GitHub" className="w-10 h-10 mr-4" />
                             </a>
                             Python Quiz Game
                         </h3>
-                        <div className='font-serif mt-2 text-sm overflow-y-auto h-full py-3'>
-                            - Developed a interactive Quiz Game in Python.
-                            - Randomly generates questions from a dictionary and displays final score upon completion
+                        <div className='font-serif mt-2 text-sm overflow-y-auto h-auto py-3'>
+                            <p className='text-left'>- Developed an interactive Quiz Game in Python.</p>
+                            <p className='text-left'>- Randomly generates questions from a dictionary and displays final score upon completion.</p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     );
 }
 
