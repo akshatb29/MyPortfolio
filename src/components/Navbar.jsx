e9d5cd0dc136b35faea5e4bb083af7bde381ed2a
akshatb29 <bhandariakshat29@gmail.com>
2024-07-25 01:19:57 +0530

    removed md navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -18,20 +18,7 @@ const Navbar = () => {
                 <li className='p-5 transition-transform transform hover:scale-110'><a href='#contact'>Contact</a></li>
 
             </ul>
-            {/* <div onClick={handleNav} className='block md:hidden'>
-                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-            </div>
 
-            <div className={nav ? 'fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-                <h1 className='text3xl primary-color m-4 scroll-smooth' >Akshats Portfolio</h1>
-                <ul className='p-8 text-2xl'>
-                    <li className='p-5 transition-transform transform hover:scale-110'><a href='#about'>About</a></li>
-                    <li className='p-5 transition-transform transform hover:scale-110'><a href='#skills'>Skills</a></li>
-                    <li className='p-5 transition-transform transform hover:scale-110'><a href='#contact'>Projects</a></li>
-                    <li className='p-5 transition-transform transform hover:scale-110'><a href='#contact'>Contact</a></li>
-                </ul>
-
-            </div> */}
         </div>
     );
 };
